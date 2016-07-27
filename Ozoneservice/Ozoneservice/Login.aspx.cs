ca1dfd8177e81beb44194fad3bcbcf045c06b404
champyuyee <l2etire_yuyee@hotmail.com>
2016-07-27 20:25:50 +0700

    lll
    jj

---
+++ Ozoneservice/Ozoneservice/Login.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ozoneservice
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Btnlogin_Click(object sender, EventArgs e)
+        {
+            string username = inputUsername.Value;
+            string password = inputPassword.Value;
+            if(username=="admin"&&password=="password@1")
+            {               
+                Response.Redirect("Mainpage.aspx");
+            }
+        }
+
+        
+
+        
+
+      
+    }
+}
---
+++ Ozoneservice/Ozoneservice/Mainpage.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site_Main.Master" AutoEventWireup="true" CodeBehind="Mainpage.aspx.cs" Inherits="Ozoneservice.WebForm2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+</asp:Content>
